54a7af8fb0cae1a2079098540920da041837e0c9
Etienne31 <etienne.maillard37@gmail.com>
2023-01-19 14:39:21 +0100

    db

--- applicationClavardageITJ/src/main/java/project/application/Manager/UserDBManager.java
+++ applicationClavardageITJ/src/main/java/project/application/Manager/UserDBManager.java
@@ -81,6 +81,7 @@ public class UserDBManager {
   public static List<Utilisateur> getListUser(String idUser) {
       Session session = Hibernate.getSessionFactory().openSession();
        String hql = "FROM Utilisateur u WHERE u.idUser = :id";
+       
      // String hql = "SELECT u FROM "+Utilisateur.class.getName()+ " u WHERE u.idUser = :id";
       Query query = session.createQuery(hql);
       query.setParameter("id", idUser);
