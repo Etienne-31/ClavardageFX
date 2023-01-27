3e3ba8c249109f89228a80a8f3f0b05129995164
Etienne-31 <90270401+Etienne-31@users.noreply.github.com>
2023-01-27 17:20:11 +0100

    travail bdd liste

--- applicationClavardageITJ/src/main/java/project/application/Manager/MessageDBManager.java
+++ applicationClavardageITJ/src/main/java/project/application/Manager/MessageDBManager.java
@@ -2,6 +2,7 @@ package project.application.Manager;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import project.application.Models.Hibernate;
 import project.application.Models.Messages;
 import project.application.Models.UserLogin;
@@ -32,10 +33,25 @@ public class MessageDBManager {
 
     }
 
-    public static List<Messages> getListMessageConv(){
-        List<Messages> listMessages;
+    public static List<Messages> getListMessageConv(String idSender ,String idReceiver){
+        List<Messages> listMessages = null;
         Session session = Hibernate.getSessionFactory().openSession();
+        String hql = " FROM Messages WHERE idSender = :sender OR idReceiver = :receiver ORDER BY timestamp";
+        try {
+            session.getTransaction().begin();
+            Query query = session.createQuery(hql);
 
+            query.setParameter("sender", idSender);
+            query.setParameter("receiver", idReceiver);
+
+            listMessages = query.list();
+            session.getTransaction().commit();
+            Hibernate.shutdown();
+        }
+        catch (Exception e){
+            session.getTransaction().rollback();
+            e.printStackTrace();
+        }
 
 
         session.close();
